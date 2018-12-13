6e6001b4b1618b0c9f555a84f562024a0c7b87d1
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-12-13 16:50:53 -0500

    commented out demo code for functions

---
+++ script.js
@@ -0,0 +1,31 @@
+// Any time we find ourselves writing the same piece of code more than once, we should probably put it in a function
+
+// Simple function that console.logs something
+
+
+// Function that takes a name as a parameter and console.logs a greeting
+
+
+
+
+
+// Function that takes an HTML string and an id and prints something to the DOM
+
+
+
+// Call the function with a random html string
+
+
+
+// Return statements
+
+// Write a function that accepts an array of taco toppings and builds an html string about the finished taco
+// Try to log the finishedTaco variable to the console outside the function! It won't work
+// Return the taco
+// Store it in a variable
+// Log the variable
+function makeATaco(shellType, toppingArray) {
+  var finishedTaco = "This is your finished taco!";
+}
+
+
